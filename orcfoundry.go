5a118d8e9bb0eeb04bf3ccda264e4df2d6166f81
Alina Mackenzie <helloalimac@gmail.com>
2018-01-21 15:32:54 -0600

    .Must enforces existence of a template

--- orcfoundry.go
+++ orcfoundry.go
@@ -31,10 +31,8 @@ func init() {
 // Render templates for the given name, template definition and data object
 func renderTemplate(w http.ResponseWriter, name string, template string, viewModel interface{}) {
 	// Ensure template exists in the map
-	tmpl, ok := templates[name]
-	if !ok {
-		http.Error(w, "Template does not exist", http.StatusInternalServerError)
-	}
+	tmpl, _ := templates[name]
+
 	err := tmpl.ExecuteTemplate(w, template, viewModel)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
